31a81a65d08652a62ece37c7f0c5d47d1a908210
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-03 22:47:15 +0800

    added firebase write comments

--- app/src/main/java/com/danielappdev/fosterships/loadingactitvity.java
+++ app/src/main/java/com/danielappdev/fosterships/loadingactitvity.java
@@ -12,6 +12,16 @@ public class loadingactitvity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loadingactitvity);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Rooms").child("room1").child("room1code");
+        myRef.setValue("1");
+        DatabaseReference my2Ref = database.getReference("Rooms").child("room2").child("room2code");
+        my2Ref.setValue("2");
+
+        DatabaseReference my3Ref = database.getReference("Participants inRooms").child("1st person PID");
+        my3Ref.setValue("2");
+
     }
 }
 
