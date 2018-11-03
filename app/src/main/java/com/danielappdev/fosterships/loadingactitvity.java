5b04561b468a56909ba77c7adc6aa12e98e8d01d
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-03 22:55:20 +0800

    added firebase write comments v2

--- app/src/main/java/com/danielappdev/fosterships/loadingactitvity.java
+++ app/src/main/java/com/danielappdev/fosterships/loadingactitvity.java
@@ -2,25 +2,19 @@ package com.danielappdev.fosterships;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class loadingactitvity extends AppCompatActivity {
 
+public class loadingactitvity extends AppCompatActivity {
+    private String android_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loadingactitvity);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Rooms").child("room1").child("room1code");
-        myRef.setValue("1");
-        DatabaseReference my2Ref = database.getReference("Rooms").child("room2").child("room2code");
-        my2Ref.setValue("2");
-
-        DatabaseReference my3Ref = database.getReference("Participants inRooms").child("1st person PID");
-        my3Ref.setValue("2");
 
     }
 }
