2fddb7d114b1058442cda6355f9aa5c877a337f6
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-04 10:14:50 +0800

    added intent for moving to second sreen

--- app/src/main/java/com/danielappdev/fosterships/loadingactitvity.java
+++ app/src/main/java/com/danielappdev/fosterships/loadingactitvity.java
@@ -1,8 +1,12 @@
 package com.danielappdev.fosterships;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,7 +28,21 @@ public class loadingactitvity extends AppCompatActivity {
         setEventCode.setValue("#CTamara");
         DatabaseReference setEventName = database.getReference("Events").child("eventName");
         setEventName.setValue("Camp Tamara");
-        }
+
+        ConstraintLayout rlayout = (ConstraintLayout) findViewById(R.id.relativeloading);
+        rlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),selectmenu.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+
 }
 
 
