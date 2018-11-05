f34da0c59b52ef0214a5e8ae632c864e20bec2d3
brianchew <brian@bchewy.com>
2018-11-06 01:09:17 +0800

    Reading the data... works!
    Some issues
    - Can't read integer...? find a workaround soon!
    - Looking for a way to save data properly as eventName cannot be read idk why!

--- app/src/main/java/com/danielappdev/fosterships/BookingActivity.java
+++ app/src/main/java/com/danielappdev/fosterships/BookingActivity.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import android.app.usage.EventStats;
 import android.content.DialogInterface;
+import android.service.autofill.Dataset;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,13 +25,17 @@ public class BookingActivity extends AppCompatActivity {
     TextView txtAdminEmail;
     TextView txtEventName;
     TextView txtNoPpl;
-
+    Button btnCheckDetails;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference defReference = database.getReference("Events"); //Initial root reference
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
-        //Load up buttons
+
+        //Look for the different ui stuff
         btnBookEvent = findViewById(R.id.btnBookEvent);
+        btnCheckDetails = findViewById(R.id.btnCheckDetails);
         txtAdminEmail = findViewById(R.id.txtadminEmail);
         txtEventName = findViewById(R.id.txtEventName);
         txtNoPpl = findViewById(R.id.txtNoPpl);
@@ -39,23 +44,33 @@ public class BookingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Event newEvent = new Event(txtEventName.getText().toString(), txtAdminEmail.getText().toString(), txtNoPpl.getText().toString());
-                int eventID = saveData(newEvent);
-                ShowDialog(eventID);
-                readData();
+                saveData(newEvent,database);
+                ShowDialog("Booking has been made. We will follow up with an email shortly!");
 
+            }
+        });
+        btnCheckDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData(defReference);
             }
         });
 
     }
-    private void readData(){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference();
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+    private void loadData(final DatabaseReference reference){
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot child : dataSnapshot.getChildren()){
-                    //do something here...
+            public void onDataChange(DataSnapshot snapshot) { //snapshot is the root reference
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    //String eventName = snapshot.getValue(String.class);
+                    String eventAdminEmail = ds.child("eventAdminEmail").getValue(String.class);
+                    String eventNoOfPpl = ds.child("eventNoOfPpl").getValue(String.class);
+
+                    //Log.d("tag1",eventName);
+                    Log.d("tag1",eventAdminEmail);
+                    Log.d("tag1",eventNoOfPpl);
                 }
+
             }
 
             @Override
@@ -63,26 +78,22 @@ public class BookingActivity extends AppCompatActivity {
 
             }
         });
-
+    }
+    private void saveData(Event e, FirebaseDatabase database) {
+        DatabaseReference refEventID = database.getReference("Events").push().child("eventID");
+        DatabaseReference referenceNoOfPpl = database.getReference("Events").push().child("eventNoOfPpl");
+        DatabaseReference referenceAdminEmail = database.getReference("Events").push().child("eventAdminEmail");
+        DatabaseReference reference = database.getReference("Events").push().child("eventID");
+        refEventID.setValue(e.getEventID());
+        referenceNoOfPpl.setValue(e.getEventExpectedNoOfPpl());
+        referenceAdminEmail.setValue(e.getEventAdminEmail());
     }
 
-    private int saveData(Event e) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        int n =generateEventID();
-        DatabaseReference refadmin = database.getReference("Events").child(e.eventName).child("eventAdminEmail");
-        refadmin.setValue(e.eventAdminEmail);
-        DatabaseReference refnoppl = database.getReference("Events").child(e.eventName).child("eventNoOfPpl");
-        refnoppl.setValue(e.eventExpectedNoOfPpl);
-        DatabaseReference refEventID = database.getReference("Events").child(e.eventName).child("eventID");
-        refEventID.setValue(n);
-        return n;
-
-    }
-    private void ShowDialog(int text){
+    private void ShowDialog(String text) {
         AlertDialog alertDialog = new AlertDialog.Builder(BookingActivity.this).create();
-        alertDialog.setTitle("Alert");
-        alertDialog.setMessage("Booking has been made!.. We will follow up with an email."+" EventID:"+text);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+        alertDialog.setTitle("Alert!!");
+        alertDialog.setMessage(text);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok!",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
@@ -90,21 +101,5 @@ public class BookingActivity extends AppCompatActivity {
                 });
         alertDialog.show();
     }
-    private void ShowDialogRead(String text){
-        AlertDialog alertDialog = new AlertDialog.Builder(BookingActivity.this).create();
-        alertDialog.setTitle("Alert");
-        alertDialog.setMessage("Event name:"+text);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        alertDialog.show();
-    }
-    private int generateEventID(){
-        Random rand = new Random();
-        int n = rand.nextInt(10000) + 1;
-        return n;
-    }
+
 }
--- app/src/main/java/com/danielappdev/fosterships/Event.java
+++ app/src/main/java/com/danielappdev/fosterships/Event.java
@@ -1,14 +1,18 @@
 package com.danielappdev.fosterships;
 
+import java.util.Random;
+
 public class Event {
-    public String eventName;
-    public String eventAdminEmail;
-    public String eventExpectedNoOfPpl;
+    private int eventID;
+    private String eventName;
+    private String eventAdminEmail;
+    private String eventExpectedNoOfPpl;
 
     public Event(){
         //instantiate
     }
     public Event(String eventName,String eventAdminEmail,String eventExpectedNoOfPpl){
+        this.eventID=generateEventID();
         this.eventName=eventName;
         this.eventAdminEmail=eventAdminEmail;
         this.eventExpectedNoOfPpl=eventExpectedNoOfPpl;
@@ -17,9 +21,17 @@ public class Event {
     public String getEventName() {
         return eventName;
     }
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
 
     public void setEventName(String eventName) {
         this.eventName = eventName;
+
     }
 
     public String getEventAdminEmail() {
@@ -36,5 +48,13 @@ public class Event {
 
     public void setEventExpectedNoOfPpl(String eventExpectedNoOfPpl) {
         this.eventExpectedNoOfPpl = eventExpectedNoOfPpl;
+    }
+
+    //Actual methods
+    private int generateEventID() {
+        Random rand = new Random();
+        int n = rand.nextInt(10000) + 1;
+        eventID = n;
+        return eventID;
     }
 }
