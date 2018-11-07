f30e758bcb5e5b8cbd594714b267384c621772cc
Meng Tuck Malcolm Tham <s10182012@connect.np.edu.sg>
2018-11-07 22:18:09 +0800

    tryna pullv2

---
+++ app/src/main/java/com/danielappdev/fosterships/Leaderboards.java
@@ -0,0 +1,13 @@
+package com.danielappdev.fosterships;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Leaderboards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboards);
+    }
+}
