486f5f0217ef5d1240116aa9387bd256a2e72726
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-07 21:07:05 +0800

    completed normaluserwaiting screen

--- app/src/main/java/com/danielappdev/fosterships/normaluserwaitingscreen.java
+++ app/src/main/java/com/danielappdev/fosterships/normaluserwaitingscreen.java
@@ -1,7 +1,14 @@
 package com.danielappdev.fosterships;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class normaluserwaitingscreen extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class normaluserwaitingscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normaluserwaitingscreen);
+
+        ConstraintLayout rlayout = (ConstraintLayout) findViewById(R.id.normaluserswaitingscreenlayout);
+        rlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),authentication.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button nexttip = (Button) findViewById(R.id.btnnextfact);
+        nexttip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tvnextfact =" A ball of glass will bounce higher than a ball of rubber.";
+                TextView tvnextfact1 = findViewById(R.id.tvfact);
+                tvnextfact1.setText(tvnextfact.toString());
+            }
+        });
     }
 }
