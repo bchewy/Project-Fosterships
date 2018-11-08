9feaae29a39a5285f9b58a211e4641d4a5291a75
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-08 17:09:43 +0800

    completed normaluserwaiting screen

--- app/src/main/java/com/danielappdev/fosterships/selectmenu.java
+++ app/src/main/java/com/danielappdev/fosterships/selectmenu.java
@@ -20,7 +20,7 @@ public class selectmenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectmenu);
-        Log.d("selectmenu", "runs on selectmenu");
+
         inviteCode = findViewById(R.id.txtInviteCode);
         btnJoin = findViewById(R.id.btnPlayer1);
         btnBook = findViewById(R.id.btnBook);
