c06002039b88a759a5aa2e60977af0f079b39d6d
brianchew <brian@bchewy.com>
2018-11-08 09:10:17 +0800

    Changed ShowDialog a little to include text and title Also documented the code a little!

--- app/src/main/java/com/danielappdev/fosterships/selectmenu.java
+++ app/src/main/java/com/danielappdev/fosterships/selectmenu.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
