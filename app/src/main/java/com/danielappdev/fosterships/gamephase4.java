3a5fa58a50a18b5ea9e99518d71ab7d699acc13f
brianchew <brian@bchewy.com>
2018-11-09 21:11:17 +0800

    Added gamephases for players 2,3,4

---
+++ app/src/main/java/com/danielappdev/fosterships/gamephase4.java
@@ -0,0 +1,13 @@
+package com.danielappdev.fosterships;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gamephase4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamephase4);
+    }
+}
