893e0919284b715d83fa81f3c05e525844705ee6
brianchew <brian@bchewy.com>
2018-11-10 21:39:38 +0800

    Loading image with glide test - doesn't work.

--- app/src/main/java/com/danielappdev/fosterships/gamephase.java
+++ app/src/main/java/com/danielappdev/fosterships/gamephase.java
@@ -1,10 +1,14 @@
 package com.danielappdev.fosterships;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -17,13 +21,21 @@ public class gamephase extends AppCompatActivity {
         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("photo1/").child("row-1-col-1.jpg");
 
 
-        ImageView image;
+        final ImageView image;
         image = findViewById(R.id.imageView);
 
-// Load the image using Glide
-        Glide.with(this /* context */)
-                .using(new FirebaseImageLoader())
-                .load(storageReference)
-                .into(image );
+        /* Load the image using Glide */
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String imageURL = uri.toString();
+                Glide.with(getApplicationContext()).load(imageURL).into(image);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
     }
 }
