e5dd27c83483e0cdcccdcfabf3ddb9aaf1abc267
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-12 20:36:25 +0800

    added waiting screen for team members who keyed in auth code

--- app/src/main/java/com/danielappdev/fosterships/authentication.java
+++ app/src/main/java/com/danielappdev/fosterships/authentication.java
@@ -1,7 +1,10 @@
 package com.danielappdev.fosterships;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class authentication extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class authentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        ConstraintLayout CS_Auth = findViewById(R.id.CS_Auth);
+
+        CS_Auth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent intent = new Intent(getApplicationContext(), BookingActivity.class);
+               // startActivity(intent);
+            }
+        });
     }
 }
