75ebb4dd88f2fb8d1e835bff4940e36e87a97852
Eshwar V <s10169036@connect.np.edu.sg>
2018-11-14 10:44:52 +0800

    Import Java files for Leaderboard Page

--- app/src/main/java/com/danielappdev/fosterships/Leaderboards.java
+++ app/src/main/java/com/danielappdev/fosterships/Leaderboards.java
@@ -1,7 +1,24 @@
 package com.danielappdev.fosterships;
 
+
+import android.app.usage.EventStats;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.service.autofill.Dataset;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.app.AlertDialog;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 
@@ -12,4 +29,6 @@ public abstract class Leaderboards extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboards);
     }
+
+
 }
