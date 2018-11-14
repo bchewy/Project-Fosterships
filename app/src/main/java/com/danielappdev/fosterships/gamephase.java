27dfbb24ff1e00cebbe79942fdb5d3fd10d7e6c5
Daniel Dharshan Ravindran <s10181158@connect.np.edu.sg>
2018-11-14 15:13:29 +0800

    added full screening of pics

--- app/src/main/java/com/danielappdev/fosterships/gamephase.java
+++ app/src/main/java/com/danielappdev/fosterships/gamephase.java
@@ -1,35 +1,53 @@
 package com.danielappdev.fosterships;
 
-import android.net.Uri;
-import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class gamephase extends AppCompatActivity {
-
+    ImageView imageview2;
+    boolean isImageFitToScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamephase);
-        LoadImageFromFirebase();
+       // LoadImageFromFirebase();
 
 
+
+        imageview2 = findViewById(R.id.imageViewgm2);
+        imageview2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isImageFitToScreen){
+                    isImageFitToScreen=false;
+                    //  imageview2.setLayoutParams(new LinearLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+                    imageview2.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+                    imageview2.setAdjustViewBounds(true);
+                }
+
+                else{
+                    // imageview2.setLayoutParams(new LinearLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+                    imageview2.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+                    imageview2.setScaleType(ImageView.ScaleType.FIT_XY);
+
+                }
+            }
+        });
     }
 
-    public void LoadImageFromFirebase() {
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("picture.png");//hardcoded "picture.png"
-        ImageView imageView = findViewById(R.id.imageView4);
-        Glide.with(getApplicationContext())
-                .load(storageReference)
-                .into(imageView);
-    }
 }
+
+//    public void LoadImageFromFirebase() {
+//        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("picture.png");//hardcoded "picture.png"
+//        ImageView imageView = findViewById(R.id.imageViewgm2);
+//        Glide.with(getApplicationContext())
+//                .load(storageReference)
+//                .into(imageView);
+//    }
--- app/src/main/java/com/danielappdev/fosterships/gamephase4.java
+++ app/src/main/java/com/danielappdev/fosterships/gamephase4.java
@@ -1,13 +1,37 @@
 package com.danielappdev.fosterships;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class gamephase4 extends AppCompatActivity {
-
+    ImageView imageview2;
+    boolean isImageFitToScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamephase4);
+
+        imageview2 = findViewById(R.id.imageViewgm2);
+        imageview2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isImageFitToScreen){
+                    isImageFitToScreen=false;
+                    //  imageview2.setLayoutParams(new LinearLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+                    imageview2.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+                    imageview2.setAdjustViewBounds(true);
+                }
+
+                else{
+                    // imageview2.setLayoutParams(new LinearLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+                    imageview2.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+                    imageview2.setScaleType(ImageView.ScaleType.FIT_XY);
+
+                }
+            }
+        });
     }
 }
