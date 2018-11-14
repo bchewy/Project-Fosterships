151957e6d8ed2d5dfd824a42ee8cf95b6a3669dc
brianchew <brian@bchewy.com>
2018-11-14 12:04:22 +0800

    Added teams database to firebase -- allows for checking if players are on the same team, and if the code is correct! - game phase!

--- app/src/main/java/com/danielappdev/fosterships/TeamLeaderAuthentication.java
+++ app/src/main/java/com/danielappdev/fosterships/TeamLeaderAuthentication.java
@@ -23,7 +23,7 @@ public class TeamLeaderAuthentication extends AppCompatActivity {
     Integer eventID;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference defReferenceTeams = database.getReference("Teams");
+    DatabaseReference defReferenceTeams = database.getReference("Teams"); //root for teams
     DatabaseReference defReference = database.getReference("Events"); //Initial root reference
 
     @Override
@@ -34,10 +34,10 @@ public class TeamLeaderAuthentication extends AppCompatActivity {
         authCode = findViewById(R.id.AuthEditCode);
         authCodeString = "#"+getRandomString(5);
         authCode.setText(authCodeString);
-        saveData(defReference,authCodeString);
+        saveData(authCodeString,eventID);
         Intent mIntent = getIntent();
         eventID = mIntent.getIntExtra("EventID", 0);
-        CreateTeams(defReferenceTeams,eventID); //take eventid from admin page --> team leader authentication.
+        CreateTeams(eventID); //take eventid from admin page --> team leader authentication.
     }
 
     private static final String ALLOWED_CHARACTERS ="0123456789qwertyuiopasdfghjklzxcvbnm";
@@ -50,22 +50,24 @@ public class TeamLeaderAuthentication extends AppCompatActivity {
             sb.append(ALLOWED_CHARACTERS.charAt(random.nextInt(ALLOWED_CHARACTERS.length())));
         return sb.toString();
     }
-    private void saveData(DatabaseReference reference,String authCodeString) {
-        reference.push();
-        String key = reference.getKey();
-        DatabaseReference referenceCode = database.getReference("AuthCodes").child(key).child("Code");
+    private void saveData(String authCodeString, Integer eventID) {
+        DatabaseReference reference = database.getReference("Events").push();
+        final String key = reference.getKey();
+        DatabaseReference referenceCode = database.getReference("AuthCodes").child(key).child("TeamAuthCode");
         DatabaseReference referenceName = database.getReference("AuthCodes").child(key).child("Name");
+//        DatabaseReference rID = database.getReference("AuthCodes").child(key).child("eventID");
+//        rID.setValue(eventID);
         referenceCode.setValue(authCodeString);
         referenceName.setValue("Team Banana");
     }
-    private void CreateTeams(DatabaseReference reference,Integer eventID){
-        reference.push();//repeated code
-        String key = reference.getKey();
-        DatabaseReference referenceName = database.getReference(key).child("Name");
-        DatabaseReference referenceEventName = database.getReference(key).child("eventID");
-        DatabaseReference referenceAnswerP1 = database.getReference(key).child("Phase1Answer");
-        referenceEventName.setValue(eventID);
-        referenceName.setValue("Team Banana");
-        referenceAnswerP1.setValue("Two Red Mushroom");
+    private void CreateTeams(Integer eventID){
+        DatabaseReference reference = database.getReference("Teams").push();
+        final String key = reference.getKey();
+        DatabaseReference rName = database.getReference("Teams").child(key).child("TeamName");
+        DatabaseReference rID = database.getReference("Teams").child(key).child("eventID");
+        DatabaseReference rAnswer = database.getReference("Teams").child(key).child("phase1Answer");
+        rID.setValue(eventID);
+        rName.setValue("Team Banana");
+        rAnswer.setValue("Two Red Mushrooms");
     }
 }
